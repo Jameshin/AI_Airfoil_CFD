f2d0c66654eb4a25702ecdd0b7f1b23df4669298
Jung-Hun Shin <shinjunghun77@gmail.com>
2022-07-14 17:22:23 +0900

    Update change_foldername.py

--- UIUC_Airfoil_Dataset_Handler/change_foldername.py
+++ UIUC_Airfoil_Dataset_Handler/change_foldername.py
@@ -4,7 +4,7 @@ import os
 path_dir = './'
 #file_list = os.listdir(path_dir)
 #file_list.sort()
-numd = 1551
+numd = 1550
 #print(numd, file_list[0])
 #f = open("list_airfoils.dat", "w")
 for i in range(138,numd+1):
