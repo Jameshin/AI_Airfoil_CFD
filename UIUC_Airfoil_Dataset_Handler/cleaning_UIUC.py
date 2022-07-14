08ec192a3a1e1383487aeb82ebe5a2cbf1e3bf72
Jung-Hun Shin <shinjunghun77@gmail.com>
2022-07-14 16:05:01 +0900

    Update cleaning_UIUC.py

--- UIUC_Airfoil_Dataset_Handler/cleaning_UIUC.py
+++ UIUC_Airfoil_Dataset_Handler/cleaning_UIUC.py
@@ -6,7 +6,6 @@ numd = len(file_list)
 #print(numd, file_list[0])
 for i in range(numd):
     with open(file_list[i],'r', encoding='cp949') as f:
-        #filename = os.path.splitext(file_list[i])[0]
         with open("../UIUC_DB_KISTI/"+file_list[i],'w') as f1:
             next(f)
             next(f)
