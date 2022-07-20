b0b7a95536160f4e35c5cca45f50ee8f51d8cb1d
Jung-Hun Shin <shinjunghun77@gmail.com>
2022-07-20 17:11:00 +0900

    Update FCDNN_Unstruc.py

--- FCDNN_ROM/FCDNN_Unstruc.py
+++ FCDNN_ROM/FCDNN_Unstruc.py
@@ -5,97 +5,58 @@
 
 import tensorflow.compat.v1 as tf
 import numpy as np
-import scipy.io
+#import scipy.io
 import time
 import sys
 import os
-import pandas as pd
-import pickle
+#import pandas as pd
 
 from CFDFunctions1 import neural_net, tf_session, mean_squared_error, relative_error
 
 tf.compat.v1.disable_eager_execution()
 
-class HFM(object):
+class DNN_Unstruc(object):
     # notational conventions
     # _tf: placeholders for input/output data and points used to regress the equations
     # _pred: output of neural network
-    # _eqns: points used to regress the equations
     # _data: input-output data
-    # _inlet: input-output data at the inlet
-    # _star: preditions
     
     def __init__(self, t_data, x_data, y_data, l_data,
                  d_data, u_data, v_data, p_data, 
-                 layers, batch_size, Pec, Rey):
+                 layers, batch_size):
         
         # specs
         self.layers = layers
         self.batch_size = batch_size
-        
-        # flow properties
-        self.Pec = Pec
-        self.Rey = Rey
 
         # base space
         noConcernsVar = 4 
         N = x_data.shape[0]
         T = t_data.shape[0]
         # data
-        #[self.t_pod_data, self.t_pod_eqns, self.a0_data, self.a1_data, self.a2_data, self.a3_data, self.a4_data, self.a5_data, self.a6_data, self.a7_data, self.a8_data, self.a9_data, self.a10_data, self.a11_data, self.a12_data, self.a13_data, self.a14_data, self.a15_data, self.a16_data, self.a17_data, self.a18_data, self.a19_data] = [t_pod_data, t_pod_eqns, a0_data, a1_data, a2_data, a3_data, a4_data, a5_data, a6_data, a7_data, a8_data, a9_data, a10_data, a11_data, a12_data, a13_data, a14_data, a15_data, a16_data, a17_data, a18_data, a19_data]
         [self.t_data, self.x_data, self.y_data, self.l_data, self.d_data, self.u_data, self.v_data, self.p_data] = [t_data, x_data, y_data, l_data, d_data, u_data, v_data, p_data]
         
         # placeholders
         [self.t_data_tf, self.x_data_tf, self.y_data_tf, self.d_data_tf, self.u_data_tf, self.v_data_tf, self.p_data_tf] = [tf.placeholder(tf.float64, shape=[None, 1]) for _ in range(7)]
-        #[self.t_pod_eqns_tf, self.t_data_tf, self.x_data_tf, self.y_data_tf, self.l_data_tf] = [tf.placeholder(tf.float64, shape=[None, 1]) for _ in range(5)]
 
-        # physics "uninformed" neural networks
+        # neural networks
         self.net_duvp = neural_net(self.t_data, self.x_data, self.y_data, layers = self.layers) #[3,12,12,12,12,12,12,12,4])
-        #print(np.array(self.t_pod_data).shape)
-        
-        # physics "informed" neural networks
-        [self.d_data_pred, 
+
+        [self.d_data_pred,
          self.u_data_pred,
          self.v_data_pred,
          self.p_data_pred] = self.net_duvp(self.t_data_tf,
                                            self.x_data_tf,
-                                           self.y_data_tf)
-
-        #[self.e1_data_pred,
-        # self.e2_data_pred,
-        # self.e3_data_pred, 
-        '''self.e1_data_pred = Euler_uComp_2D(self.d_data_pred, 
-                                               self.u_data_pred,
-                                               self.v_data_pred,
-                                               self.p_data_pred,
-                                               self.t_data_tf,
-                                               self.x_data_tf,
-                                               self.y_data_tf,
-                                               self.Pec,
-                                               self.Rey)
-        '''
-        # gradients required for the lift and drag forces
-        #[self.u_x_eqns_pred,
-        # self.v_x_eqns_pred,
-        # self.u_y_eqns_pred,
-        # self.v_y_eqns_pred] = Gradient_Velocity_2D(self.u_eqns_pred,
-        #                                            self.v_eqns_pred,
-        #                                            self.x_eqns_tf,
-        #                                            self.y_eqns_tf)
+                                           self.y_data_tf)   
         
         # loss
         self.loss = mean_squared_error(self.d_data_pred, self.d_data_tf) + \
                     mean_squared_error(self.u_data_pred, self.u_data_tf) + \
                     mean_squared_error(self.v_data_pred, self.v_data_tf) + \
-                    mean_squared_error(self.p_data_pred, self.p_data_tf) #+ \
-                    #mean_squared_error(self.e1_data_pred, 0.0) #+ \
-                    #mean_squared_error(self.e2_data_pred, 0.0) + \
-                    #mean_squared_error(self.e3_data_pred, 0.0) #+ \
-                    #mean_squared_error(self.e4_data_pred, 0.0)
-                    #mean_squared_error(self.p_data_pred-tf.reduce_mean(self.p_data_pred), self.p_data_tf-tf.reduce_mean(self.p_data_tf)) #+ \
+                    mean_squared_error(self.p_data_pred, self.p_data_tf) 
         
         # optimizers
-        self.global_step = tf. Variable(0, trainable = False, name='global_step')
+        self.global_step = tf.Variable(0, trainable = False, name='global_step')
         self.learning_rate = tf.placeholder(tf.float32, shape=[])
         self.optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
         self.train_op = self.optimizer.minimize(self.loss, global_step=self.global_step)
@@ -192,7 +153,8 @@ if __name__ == "__main__":
         wt = 1000 
         d_inf = 1.225
         U_inf = 0.005*343
-        sim_data_path = "D:\\JupyterNBook\\HFM-master\\Source\\pinn_POD\\2D_uComp\\result_Ma0.4_AOA15\\"
+        data_path = "D:\\2D_uComp\\result_Ma0.4_AOA15\\"
+        sim_data_path = "C:\\Users\\KISTI\\Documents\\Sim\\AI_Apps\\AI_Airfoil_CFD-main\\"
         Tecplot_header_in = "variables =x, y, rho, u, v, p, m"
         Tecplot_header_out = "variables =x, y, rho, u, v, p, m"
         n_xy = zone1_n*2
@@ -204,15 +166,14 @@ if __name__ == "__main__":
         filenames = []
         Ntime = 0
         for i in range(0, numd):
-            filenames.append(sim_data_path+"mid_result_"+str(initial_time+i*inc_time).rjust(5,'0')+".rlt")
+            filenames.append(data_path+"mid_result_"+str(initial_time+i*inc_time).rjust(5,'0')+".rlt")
             Ntime += 1
         #print(Ntime, filenames)
         t_star = np.arange(initial_time, initial_time+numd*inc_time*2, inc_time*2)*dt # 1xT(=1)
         ###
         #perform coefficient interpolation here, using numpy for it
         total_steps = 20
-        #input_design = [251 + x for x in range(total_steps)]
-        input_times = np.arange(initial_time+inc_time, initial_time+inc_time+numd*inc_time*2, inc_time*2)*dt
+        infer_times = np.arange(initial_time+inc_time, initial_time+inc_time+numd*inc_time*2, inc_time*2)*dt
         noConcernVar = 5
 
         saved_npz = np.load("./array_Unst_21.npz")
@@ -238,10 +199,7 @@ if __name__ == "__main__":
         #label[idx_x_sur] = 1
         #label[idx_x_sur2] = 3
         #label[idx_x_sur3] = 4
-        #t_pod_data = read_times[:,None]
         T_star = np.tile(t_star, (N,1))
-        #X_star = np.tile(xydata[:,0], (T,1))
-        #Y_star = np.tile(xydata[:,1], (T,1))
         L_star = np.tile(label, (T,1))
         d_data = DC_star.T.flatten()[:,None]
         u_data = UC_star.T.flatten()[:,None]
@@ -251,24 +209,19 @@ if __name__ == "__main__":
         x_data = XE_star.T.flatten()[:,None]
         y_data = YE_star.T.flatten()[:,None]
         l_data = L_star.flatten()[:,None]
-        t_pod_eqns =  input_times[:,None]
-        T_eqns = input_times.shape[0]
-        N_eqns = N        
         print(p_data.shape, t_data.shape, x_data.shape)
     
         #sys.stdout = open('stdout.txt', 'w')
         # Training
-        model = HFM(t_data, x_data, y_data, l_data, 
+        model = DNN_Unstruc(t_data, x_data, y_data, l_data, 
                     d_data, u_data, v_data, p_data,  
-                    layers, batch_size, Pec = 1000, Rey = 10)
+                    layers, batch_size)
 
         model.train(total_time = 1, learning_rate=1e-2)
-
-        #F_D, F_L = model.predict_drag_lift(t_star)
     
         # Test Data
-        t_pod_test = input_times
-        T_test = np.tile(t_pod_test, (N,1))
+        t_rom_test = infer_times
+        T_test = np.tile(t_rom_test, (N,1))
     
         # Prediction
         #a_pred = model.predict(t_test, x_data, y_data)
@@ -295,4 +248,3 @@ if __name__ == "__main__":
             print('Error d: %e, u: %e, v: %e, p: %e' % (error_d, error_u, error_v, error_p))
         #sys.stdout.close()
 #"""  
-
